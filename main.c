32bd561bdf2f8108818678b4a09e08e3e3d08f44
masd28 <masd28@lehigh.edu>
2024-10-30 16:07:46 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,50 +1,27 @@
-#include <msp430.h> 
+#include <msp430.h>
+#include "GPIO_Driver.h".
 
-int cal_add(int a, int b);
-int cal_sub(int a, int b);
-int cal_mult(int a, int b);
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;
 
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    gpioInitOut(1, BIT0);
+    gpioInitOut(6, BIT6);
 
-    int num1, num2;   // Declare two integer variables 'num1' and 'num2'.
-    int sum, sub, mult, mod, power;   // Declare variables to store the results of arithmetic operations.
-    float div;   // Declare a float variable 'div' to store the result of division.
+    gpioInitIn(4, BIT1);
+    gpioInitIn(2, BIT3);
 
-    num1=10;
-    num2=5;
-
-    sum = cal_add (num1,num2);
-    sub = cal_sub (num1,num2);
-
-    //Performs all arithmetic operations
-    //sum = num1 + num2;   // Calculate the sum of 'num1' and 'num2'.
-    //sub = num1 - num2;   // Calculate the difference between 'num1' and 'num2'.
-    mult = mult(num1, num2);  // Calculate the product of 'num1' and 'num2'.
-    div = (float)num1 / num2;   // Calculate the division of 'num1' by 'num2' and cast the result to a float.
-    mod = num1 % num2;   // Calculate the modulus of 'num1' and 'num2'.
-    power = num1*num1*num1*num1*num1; //// Calculate powrr of 'num1' is 'num2'.
-
-    while(1);
-
-    return 0;   // Return 0 to indicate successful execution of the program.
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                           // to activate previously configured port settings
+    unsigned char value = 0;
+    while(1){
+        value = gpioInitStatus(4, BIT1);
+        if(value == 0x00) {
+            _delay_cycles(5000);
+            gpioWrite(1, BIT0, 1);
+        } else {
+            gpioWrite(1, BIT0, 0);
+        }
+       _delay_cycles(5000);
+    }
 }
 
-int cal_add(int a, int b) {
-    int result_add;
-    result_add = a+b;
-    return result_add;
-}
-
-
-int cal_sub(int a, int b) {
-    int result_sub;
-    result_sub = a-b;
-    return result_sub;
-}
-
-int cal_mult(int a, int b) {
-    int result_mult;
-    result_mult = a*b;
-    return result_mult;
-}
